5e43e2379f289f4fdc2db61926adc7dc714c824b
Tinyiko Hlungwani <tinyiko389@gmail.com>
2019-11-19 10:36:57 +0200

    initial commit

---
+++ oddNumbers.js
@@ -0,0 +1,15 @@
+function oddNumbers(){
+    startpoint = 0;
+
+    if (startpoint / 2 != 0){
+        startpoint = startpoint + 1;
+        console.log(startpoint);
+    }
+
+    // while (startpoint <10){
+    //     startpoint = startpoint + 2;
+    //     console.log(startpoint);
+    // }
+
+}
+oddNumbers();
